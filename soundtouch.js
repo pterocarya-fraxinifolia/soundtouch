84d5e220c8ee9a10f126e1c838f782f5e1dc37c7
Bernd Franke <dr.bernd.franke@gmail.com>
2018-05-02 14:33:15 +0200

    eslint support and reformatting

--- soundtouch.js
+++ soundtouch.js
@@ -21,21 +21,21 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-'use strict';
-const DeviceMasterName = 'Working';
-const WellKnownDevices = new Array('192.168.0.90', '192.168.0.91', '192.168.0.92');
+"use strict";
+const DeviceMasterName = "Working";
+const WellKnownDevices = new Array("192.168.0.90", "192.168.0.91", "192.168.0.92");
 
-var requestPromise = require('request-promise-native');
-var util = require('util');
-var parseStringPromise = util.promisify(require('xml2js').parseString);
-var ssdp = require('node-ssdp');
+var requestPromise = require("request-promise-native");
+var util = require("util");
+var parseStringPromise = util.promisify(require("xml2js").parseString);
+var ssdp = require("node-ssdp");
 
 function exceptionHandler(t, e, rethrow = false) {
     if(e) {
-	console.log('#### ' + t + ' failed! ####\n' + e + '\n\n');
-	if(rethrow) {
+        console.log("#### " + t + " failed! ####\n" + e + "\n\n");
+        if(rethrow) {
 	    throw(e);
-	}
+        }
     }
     return e ? Promise.reject(e) : Promise.resolve(t);
 }
@@ -43,10 +43,10 @@ function exceptionHandler(t, e, rethrow = false) {
 function delay(time, value) {
     // console.log("delay: ", value);
     return new Promise(function (resolve) {
-	setTimeout(() => {
+        setTimeout(() => {
 	    // console.log(" ... finished"); 
-	    resolve(value)
-	}, time);
+	    resolve(value);
+        }, time);
     });
 }
 
@@ -60,18 +60,18 @@ function delay(time, value) {
 class SoundTouchDevice {
 
     constructor(address) {
-	this.address = address;
-	this.exceptionHandler = exceptionHandler;
-	this.valid = false;
+        this.address = address;
+        this.exceptionHandler = exceptionHandler;
+        this.valid = false;
     }
 
     init() {
-	try {
-	    this.uri = 'http://' + this.address + ':8090';
-	    return requestPromise(this.uri + '/info')
-		.then(parseStringPromise, e => Promise.reject(e))
-		.then(result => {
-		    this.deviceID = result.info['$'].deviceID;
+        try {
+	    this.uri = "http://" + this.address + ":8090";
+	    return requestPromise(this.uri + "/info")
+                .then(parseStringPromise, e => Promise.reject(e))
+                .then(result => {
+		    this.deviceID = result.info["$"].deviceID;
 		    this.name = result.info.name[0];
 		    this.type = result.info.type[0];
 		    this.MAC = result.info.networkInfo[0].macAddress[0];
@@ -79,170 +79,170 @@ class SoundTouchDevice {
 		    this.IP = result.info.networkInfo[0].ipAddress[0];
 		    this.IP1 = result.info.networkInfo[1].ipAddress[0];
 		    this.valid = true;
-		})
-		.catch(e => Promise.reject(e));
-	}
-	catch (e) {
-	    return this.exceptionHandler('init() ', e);
-	}
+                })
+                .catch(e => Promise.reject(e));
+        }
+        catch (e) {
+	    return this.exceptionHandler("init() ", e);
+        }
     }
 
     volume(vol) {
-	return requestPromise({uri: 'http://' + this.address + ':8090/volume', method: 'POST', body: '<volume>' + vol + '</volume>\n\n'})
+        return requestPromise({uri: "http://" + this.address + ":8090/volume", method: "POST", body: "<volume>" + vol + "</volume>\n\n"})
 	    .then(() => Promise.resolve(vol));
     }
 
     volumeFadeIn(volumeEnd, volumeBegin = 0, span = 20000) {
-	const smax = Math.abs(Math.floor(volumeEnd - volumeBegin));
-	const timeStep = span / smax;
-	const volStep = (volumeEnd - volumeBegin) / smax;
+        const smax = Math.abs(Math.floor(volumeEnd - volumeBegin));
+        const timeStep = span / smax;
+        const volStep = (volumeEnd - volumeBegin) / smax;
 
-	var p = Promise.resolve(volumeBegin);
-	for(let s = 0; s < smax; s++) {
+        var p = Promise.resolve(volumeBegin);
+        for(let s = 0; s < smax; s++) {
 	    p = p.then(vol => this.volume(vol))
-		.then(vol => delay(timeStep, vol + 1));
-	}
-	return p;
+                .then(vol => delay(timeStep, vol + 1));
+        }
+        return p;
     }
 
     key(key) {
-	return requestPromise({uri: this.uri + '/key', method: 'POST', body: '<key state=\"press\" sender =\"Gabbo\">' + key + '</key>'})
+        return requestPromise({uri: this.uri + "/key", method: "POST", body: "<key state=\"press\" sender =\"Gabbo\">" + key + "</key>"})
 	    .then(() => delay(200))
-	    .then(() => requestPromise({uri: this.uri + '/key', method: 'POST', body: '<key state=\"release\" sender =\"Gabbo\">' + key + '</key>'}))
+	    .then(() => requestPromise({uri: this.uri + "/key", method: "POST", body: "<key state=\"release\" sender =\"Gabbo\">" + key + "</key>"}))
 	    .then(() => delay(200))
-	    .then(() => Promise.resolve(key))
+	    .then(() => Promise.resolve(key));
     }
 
     keySequence(keys) {
-	var p = Promise.resolve(0);
-	for(let i = 0; i < keys.length; i++) {
+        var p = Promise.resolve(0);
+        for(let i = 0; i < keys.length; i++) {
 	    p = p.then(() => this.key(keys[i]));
-	}
-	return p;
+        }
+        return p;
     }
 }
 
 
 class SoundTouch {
     constructor() {
-	this.devices = new Map();
-	this.exceptionHandler = exceptionHandler;
+        this.devices = new Map();
+        this.exceptionHandler = exceptionHandler;
     }
 
     key(key) {
-	var p = Promise.resolve(key);
-	for(const [address, device] of this.devices) {
+        var p = Promise.resolve(key);
+        for(const [address, device] of this.devices) {
 	    p = p.then(() => device.key(key));
-	}
-	return p;
+        }
+        return p;
     }
 
     volume(vol) {
-	var p = Promise.resolve(0);
-	for(const [address, device] of this.devices) {
+        var p = Promise.resolve(0);
+        for(const [address, device] of this.devices) {
 	    p = p.then(() => device.volume(vol));
-	}
-	return p;
+        }
+        return p;
     }
 
     volumeFadeIn(volumeEnd, volumeBegin = 0, span = 20000) {
-	const smax = Math.abs(Math.floor(volumeEnd - volumeBegin));
-	const timeStep = span / smax;
-	const volStep = (volumeEnd - volumeBegin) / smax;
+        const smax = Math.abs(Math.floor(volumeEnd - volumeBegin));
+        const timeStep = span / smax;
+        const volStep = (volumeEnd - volumeBegin) / smax;
 
-	var p = Promise.resolve(volumeBegin);
-	for(let s = 0; s < smax; s++) {
+        var p = Promise.resolve(volumeBegin);
+        for(let s = 0; s < smax; s++) {
 	    for(const [address, device] of this.devices) {
-		p = p.then(vol => device.volume(vol));
+                p = p.then(vol => device.volume(vol));
 	    }
 	    p = p.then(vol => delay(timeStep, vol + 1));
-	}
+        }
 
-	return p;
+        return p;
     }
 
     addDevice(address) {
-	try {
+        try {
 	    // console.log('addDevice(): ', address);
 	    var device = new SoundTouchDevice(address);
 	    return device.init()
-		.then(() => {
+                .then(() => {
 		    if(device.valid && !this.devices.has(address)) {
-			this.devices.set(address, device);
-			if(!this.deviceMaster || device.name === DeviceMasterName) {
+                        this.devices.set(address, device);
+                        if(!this.deviceMaster || device.name === DeviceMasterName) {
 			    this.deviceMaster = device;
-			}
+                        }
 		    }
 		    return Promise.resolve(device);
-		})
-		.catch(() => Promise.resolve());
-	}
-	catch (e) {
-	    return this.exceptionHandler('addDevice()', e);
-	}
+                })
+                .catch(() => Promise.resolve());
+        }
+        catch (e) {
+	    return this.exceptionHandler("addDevice()", e);
+        }
     }
 
     groupZone() {
-	var zoneInfo = '', zoneMaster = '', zoneMember = '';
+        var zoneInfo = "", zoneMaster = "", zoneMember = "";
 
-	for(let [address, device] of this.devices) {
+        for(let [address, device] of this.devices) {
 	    if(device === this.deviceMaster) {
-		zoneMaster = '<zone master=\"' + device.MAC + '\" senderIPAddress=\"' + device.IP + '\">';
+                zoneMaster = "<zone master=\"" + device.MAC + "\" senderIPAddress=\"" + device.IP + "\">";
 	    }
-	    zoneMember += '<member ipaddress=\"' + device.IP + '\">' + device.MAC + '</member>';
-	}
-	;
+	    zoneMember += "<member ipaddress=\"" + device.IP + "\">" + device.MAC + "</member>";
+        }
+        ;
 
-	zoneInfo = zoneMaster + zoneMember + '</zone>';
+        zoneInfo = zoneMaster + zoneMember + "</zone>";
 
-	// for ZoneMaster
-	var p = requestPromise({uri: this.deviceMaster.uri + '/setZone', method: 'POST', body: zoneInfo});
+        // for ZoneMaster
+        var p = requestPromise({uri: this.deviceMaster.uri + "/setZone", method: "POST", body: zoneInfo});
 
-	// for ZoneSlaves
-	for(var [address, device] of this.devices) {
+        // for ZoneSlaves
+        for(var [address, device] of this.devices) {
 	    if(device !== this.deviceMaster) {
-		p = p.then(() => {
-		    requestPromise({uri: device.uri + '/addZoneSlave', method: 'POST', body: zoneInfo});
-		});
+                p = p.then(() => {
+		    requestPromise({uri: device.uri + "/addZoneSlave", method: "POST", body: zoneInfo});
+                });
 	    }
-	}
+        }
 
-	return p;
+        return p;
     }
 
     detect() {
-	try {
+        try {
 	    WellKnownDevices.forEach(this.addDevice);
 
 	    var client = new ssdp.Client();
-	    client.on('response', (headers, statusCode, rinfo) => this.addDevice(rinfo.address));
+	    client.on("response", (headers, statusCode, rinfo) => this.addDevice(rinfo.address));
 	    return Promise.resolve(0)
-		.then(() => {
-		    client.search('urn:schemas-upnp-org:device:MediaRenderer:1');
+                .then(() => {
+		    client.search("urn:schemas-upnp-org:device:MediaRenderer:1");
 		    return delay(2000);
-		})
-		.then(() => {
-		    client.search('urn:schemas-upnp-org:device:MediaRenderer:1');
+                })
+                .then(() => {
+		    client.search("urn:schemas-upnp-org:device:MediaRenderer:1");
 		    return delay(2000);
-		})
-		.then(() => {
-		    client.search('urn:schemas-upnp-org:device:MediaRenderer:1');
+                })
+                .then(() => {
+		    client.search("urn:schemas-upnp-org:device:MediaRenderer:1");
 		    return delay(2000);
-		})
-		.then(() => {
+                })
+                .then(() => {
 		    if(this.deviceMaster && (this.deviceMaster.name !== DeviceMasterName)) {
-			for(let device of this.devices) {
+                        for(let device of this.devices) {
 			    if(device.name === DeviceMasterName) {
-				this.deviceMaster = device;
+                                this.deviceMaster = device;
 			    }
-			}
+                        }
 		    }
 		    return Promise.resolve(this.deviceMaster);
-		});
-	}
-	catch (e) {
-	    return this.exceptionHandler('detect()', e);
-	}
+                });
+        }
+        catch (e) {
+	    return this.exceptionHandler("detect()", e);
+        }
     }
 }
 
